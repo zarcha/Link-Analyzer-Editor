6d393e4e0dd159f58b2ec1bea3eb16e9b32c6c20
Your Name <you@example.com>
2025-01-21 16:09:17 -0600

    update

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,5 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
-  base: '/Link-Analyzer-Editor/',
   preprocess: vitePreprocess(),
 }
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Link-Analyzer-Editor/",
   plugins: [svelte()],
 })
